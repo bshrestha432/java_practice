74d8e17bde7af8b1061ba689b2492522d610b49d
bshrestha432 <bshrestha@rrc.ca>
2025-05-08 13:41:15 -0500

    Display multiple value in a single line

--- practice.js
+++ practice.js
@@ -1,5 +1,11 @@
 alert ('hello, world');
 
 //displays the value of age in the console.
+//let age = 40;
+//console.log(age);
+
+//display multiple value in the same line.
 let age = 40;
-console.log(age);
+let year = 2025;
+
+console.log(age, year);
